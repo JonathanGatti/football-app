43eb9a1d9198b2f361e90f2ef9eaf3af69cf57b0
jonathan gatti <jonathangatti2@gmail.com>
2020-10-05 19:58:15 +0100

    made config file

--- src/SearchPlayerForm.js
+++ src/SearchPlayerForm.js
@@ -3,19 +3,20 @@ import axios from 'axios';
 import useSearchPlayerForm from './hooks/useSearchPlayerForm';
 import PlayersList from './PlayersList'
 import TextField from '@material-ui/core/TextField';
-const api_key = '&APIkey=909f31a51bca52bfeb7d77007168e1bf1f2c104cc0976263f1300ddc2f7f6826';
+import config from './config'
+
 const url = 'http://api-football-v1.p.rapidapi.com/v2/players/search/';
-const key ='89f56fc814mshaffde0d4f2ea23cp144e32jsnf8dcdcfa56ff'
 
 function SearchPlayerForm() {
-  const [val, handleChange, reset] = useSearchPlayerForm('')
-  const [players, setPlayers] = useState('')
+  const [val, handleChange, reset] = useSearchPlayerForm('');
+  const [players, setPlayers] = useState('');
+  
   async function getData(name){
     try {
       let res = await axios.get(`${url}${name}`, {
         "headers": {
           "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
-          "x-rapidapi-key": key,
+          "x-rapidapi-key": config.api_key,
           "useQueryString": true
         }
     })
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const config = {
+  api_key: '89f56fc814mshaffde0d4f2ea23cp144e32jsnf8dcdcfa56ff'
+}
+
+export default config
