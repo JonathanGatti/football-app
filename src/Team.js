a2eaa81415e7f3d1dac2deccad2185e76b29e66d
jonathan gatti <jonathangatti2@gmail.com>
2020-10-10 18:18:11 +0100

    displayed players dynamically added background image

--- src/Team.js
+++ src/Team.js
@@ -1,21 +1,51 @@
 import React, { useContext } from 'react';
+import background from './imgs/background.jpg'
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Player from './Player';
 import { TeamContext } from './contexts/TeamContexts';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundImage: `url(${background})`,
+    width: '100vw',
+    height: '100vh'
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 function Team(){
+  const classes = useStyles();
   const {team} = useContext(TeamContext)
+  const getType = (type) => {
+    switch(type){
+      case 'gk':
+        return 12
+      case 'def':
+        return 3
+      case 'cc':
+          return 4 
+      case 'att':
+          return 4
+    }
+  }
   return(
-      <Paper>
-        <Grid container>
+      <Paper className={classes.root}>
+        <Grid container spacing={4}>
         {team.map(player => (
-          <Grid item>
-            <Player player={player}/>
+          <Grid item xs={getType(player.type)}>
+            <Paper className={classes.paper}>
+              <Player player={player}/>
+            </Paper>
           </Grid>
-        ))
-      }
+          ))
+        }
         </Grid>
       </Paper>
   )
--- src/contexts/TeamContexts.js
+++ src/contexts/TeamContexts.js
@@ -4,16 +4,16 @@ export const TeamContext = createContext();
 
 const players = [
   {type: 'gk', name: 'buffon', age: 41, team: 'juventus'},
-  {type: 'td', name: 'danilo', age: 28, team: 'juventus'},
-  {type: 'dcd', name: 'bonucci', age: 33, team: 'juventus'},
-  {type: 'dcs', name: 'chiellini', age: 35, team: 'juventus'},
-  {type: 'dcd', name: 'alex sandro', age: 30, team: 'juventus'},
-  {type: 'cd', name: 'mcKenny', age: 22, team: 'juventus'},
+  {type: 'def', name: 'danilo', age: 28, team: 'juventus'},
+  {type: 'def', name: 'bonucci', age: 33, team: 'juventus'},
+  {type: 'def', name: 'chiellini', age: 35, team: 'juventus'},
+  {type: 'def', name: 'alex sandro', age: 30, team: 'juventus'},
+  {type: 'cc', name: 'mcKenny', age: 22, team: 'juventus'},
   {type: 'cc', name: 'bentancur', age: 23, team: 'juventus'},
-  {type: 'cs', name: 'rabiot', age: 26, team: 'juventus'},
-  {type: 'ad', name: 'kuluvesky', age: 20, team: 'juventus'},
+  {type: 'cc', name: 'rabiot', age: 26, team: 'juventus'},
+  {type: 'att', name: 'kuluvesky', age: 20, team: 'juventus'},
   {type: 'att', name: 'dybala', age: 28, team: 'juventus'},
-  {type: 'al', name: 'ronaldo', age: 35, team: 'juventus'},
+  {type: 'att', name: 'ronaldo', age: 35, team: 'juventus'},
 ]
 
 export function TeamContextProvider(props) {
